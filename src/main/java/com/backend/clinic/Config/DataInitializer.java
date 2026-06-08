d1fde45ceac3a3b2bdbc74c0c8a9c5828e618251
Tienzbb <tvt2004nd@gmail.com>
2026-06-08 16:06:57 +0700

    fix trung lap data

--- src/main/java/com/backend/clinic/Config/DataInitializer.java
+++ src/main/java/com/backend/clinic/Config/DataInitializer.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("configDataInitializer")
 @RequiredArgsConstructor
 @Slf4j
 public class DataInitializer implements CommandLineRunner {
